a7a87f1276ea3c584fdf3065f861584826e7fb62
Leo Bogert <github@leo.bogert.de>
2013-06-20 00:45:04 +0200

    Add matric-multiply-simple-debug to compile.sh

--- compile.sh
+++ compile.sh
@@ -4,4 +4,7 @@ set -o errexit
 module purge
 module add gcc/4.7.0
 module add mpi/openmpi/1.6.1/gcc_4.7.0
+
 mpicc matrix-multiply.c -std=c99 -o matrix-multiply
+mpicc matrix-multiply-simple-debug.c -std=c99 -o matrix-multiply-simple-debug
+
